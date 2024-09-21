2901becb278f0755563fb3f6268fcd6e40dc64df
Joan-112 <Oshabahebwajoan8@gmail.com>
2024-09-21 04:00:44 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<?php
+
+$name=" OSHABAHEBWA JOAN";
+$DOB="11/09/2002";
+$Address=" RUKIGA";
+$CYear=2024;
+$BYear=2002;
+$Age=$CYear-$BYear;
+echo"Name: ".$name. "<br>";
+echo"Date Of Birth : ".$DOB."<br>";
+echo"Home Address:".$Address."<br>";
+echo"Welcome home: ".$name."<br>";
+echo"You are now , ".$Age." Years old";
+/* DOB represents date of birth,
+CYEAR means current year
+BYEAR means year of birth*/
+?> 
+
